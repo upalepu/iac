b437af116d7770a5bd99d6b24bd38bdc8e627d5f
Uma Palepu <umap@crosslaketech.com>
2018-05-29 23:57:57 -0700

    Added additional code to make the script solid.

--- helpers/setupterraform.sh
+++ helpers/setupterraform.sh
@@ -4,7 +4,14 @@
 # Then uses wget to download the zip file and unzips it. 
 #
 TERRAFORMINSTALLLOCATION="/usr/local/bin"
-if [[ -e "$TERRAFORMINSTALLLOCATION" ]]; then
+
+function finish() { # finish. This gets called when program exits (whether it is with error or normal exit)
+    echo -e "Cleaning up terraform zip file [$TERRAFORMZIP] ..."
+    if [[ -e "./$TERRAFORMZIP" ]]; then rm ./$TERRAFORMZIP &>/dev/null; fi
+} 
+trap finish EXIT
+
+if [[ -e "$TERRAFORMINSTALLLOCATION/terraform" ]]; then
     echo -e "Removing existing terraform ..."
     sudo rm $TERRAFORMINSTALLLOCATION/terraform &>/dev/null
 fi 
@@ -13,28 +20,40 @@ wget https://releases.hashicorp.com/terraform &>/dev/null
 if (($?)); then echo -e "Could not figure out latest version of terraform. Install it manually!"; exit 1; fi
 cat terraform | grep -Eo "/[0-9]*\.[0-9]*\.[0-9]*" | grep -Eo "[0-9]*\.[0-9]*\.[0-9]*" > tmp
 read -e LATESTTERRAFORMVERSION < tmp
-echo -e "Latest Terraform version is [$LATESTTERRAFORMVERSION]"
 echo -e "Cleaning up tmp files ..."
 if [[ -e "./terraform" ]]; then rm ./terraform &>/dev/null; fi
 if [[ -e "./tmp" ]]; then rm ./tmp > /dev/null; fi
+echo -e "Latest Terraform version is [$LATESTTERRAFORMVERSION]"
 TERRAFORMZIP="terraform_""$LATESTTERRAFORMVERSION""_linux_amd64.zip"
 echo -e "Downloading latest terraform file [$TERRAFORMZIP] ..." 
 wget https://releases.hashicorp.com/terraform/$LATESTTERRAFORMVERSION/$TERRAFORMZIP &>/dev/null
 if (($?)); then echo -e "Could not download [$TERRAFORMZIP]. Install it manually!"; exit 1; fi
+isunzip=$(which unzip)
+if [[ "$isunzip" == "" ]]; then 
+    echo -e "unzip not found. Installing ..."
+    sudo apt-get install -y zip unzip &>/dev/null
+fi
 unzip ./$TERRAFORMZIP &>/dev/null
-sudo mv terraform $TERRAFORMINSTALLLOCATION &>/dev/null
+if [[ ! -e "terraform" ]]; then echo -e "unzip failed to unzip terraform"; fi
+if [[ ! -d "$TERRAFORMINSTALLLOCATION" ]]; then sudo mkdir "$TERRAFORMINSTALLLOCATION"; fi
+sudo mv ./terraform $TERRAFORMINSTALLLOCATION
+if (($?)); then 
+    echo -e "Failed to move [terraform] to [$TERRAFORMINSTALLLOCATION.]. Stopping operation."
+    exit 1
+else
+    echo -e "Moved [terraform] to [$TERRAFORMINSTALLLOCATION.]"
+fi 
 echo -e "Checking if [$TERRAFORMINSTALLLOCATION] is in the PATH ..."
-env | grep -Eoc "$TERRAFORMINSTALLLOCATION"
-if ((!$?)); then 
+INPATH=$(env | grep -Eoc "$TERRAFORMINSTALLLOCATION")
+if ((!$INPATH)); then 
     echo -e "Could not find [$TERRAFORMINSTALLLOCATION] in the PATH. Adding ..."
     export PATH="$TERRAFORMINSTALLOCATION:$PATH"; 
+else
+    echo -e "Found [$TERRAFORMINSTALLLOCATION] in the PATH!"
 fi
-
-echo -e "Cleaning up terraform zip file [$TERRAFORMZIP] ..."
-if [[ -e "./$TERRAFORMZIP" ]]; then rm ./$TERRAFORMZIP &>/dev/null; fi
 terraform --version
 if (($?)); then 
     echo -e "Terraform installation didn't complete! Install it manually!"
 else 
-    echo -e "Terraform [$TERRAFORMZIP] installed successfully!"
+    echo -e "$(terraform --version) installed successfully from [$TERRAFORMZIP]!"
 fi
