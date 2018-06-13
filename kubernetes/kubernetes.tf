937b40f2ee9751325c5f0e3e51b36083888e9efc
Uma Palepu <umap@crosslaketech.com>
2018-06-12 23:55:11 -0700

    Removed incorrect dependency on data.

--- kubernetes/kubernetes.tf
+++ kubernetes/kubernetes.tf
@@ -10,7 +10,6 @@ data "aws_route53_zone" "hz" {
     name = "${var.k8scfg["parm_domain"]}"
 }
 resource "aws_route53_zone" "subhz" {
-    depends_on = [ "aws_route53_zone.hz" ] 
     name = "${var.k8scfg["parm_subdomain"]}.${var.k8scfg["parm_domain"]}"
     comment = "${var.k8scfg["parm_comment"]}"
     tags {
@@ -113,47 +112,52 @@ resource "null_resource" "k8scluster" {
     provisioner "local-exec" {
         when = "create"
         # Using heredoc syntax for running multiple cmds
-        # First we copy the export commands for the env variables for use by kops into the .bashrc file. 
-        # Then we create a public key for ssh access by kops to the various kops systems. 
+        # First we check to see if a public key for ssh access by kops is already present. If so we delete it.  
+        # Now we create a public key for ssh access by kops to the various kops systems. 
         # NOTE: kops assumes a key-pair id_rsa in the .ssh directory. So the ssh-keygen cmd is important.  
         # admin is the user name required for Debian. (Seems like kops defaults to using debian for its cluster machines)
+        # Then we create the cluster. 
+        # Then we copy the export commands for the env variables for use by kops into the .bashrc file. 
         #   
         command = <<CMD
-if [[ ! -e ~/.bashrc ]]; then touch ~/.bashrc; fi
-echo -e "export NAME=${null_resource.k8scluster.triggers.k8sc_name}" >> ~/.bashrc
-echo -e "export KOPS_STATE_STORE=${null_resource.k8scluster.triggers.k8sc_s3b_name}" >> ~/.bashrc
+if [[ -e ~/.ssh/id_rsa || ~/.ssh/id_rsa.pub ]]; then rm ~/.ssh/id_rs*; fi
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa; if (($?)); then exit 1; fi
 kops create cluster \
 --cloud=aws \
 --name=${null_resource.k8scluster.triggers.k8sc_name} \
---state=${null_resource.k8scluster.triggers.k8sc_s3b_name} \
---zones=${var.k8scfg.parm_region}a \
---node-count=${var.k8scfg.parm_nodes} \
---node-size=${var.k8scfg.parm_nodetype} \
---master-size=${var.k8scfg.parm_mastertype} \
+--state=s3://${null_resource.k8scluster.triggers.k8sc_s3b_name} \
+--zones=${var.k8scfg["parm_region"]}a \
+--node-count=${var.k8scfg["parm_nodes"]} \
+--node-size=${var.k8scfg["parm_nodetype"]} \
+--master-size=${var.k8scfg["parm_mastertype"]} \
 --dns-zone=${null_resource.k8scluster.triggers.k8sc_name}
 if (($?)); then exit 1; fi
+if [[ ! -e ~/.bashrc ]]; then touch ~/.bashrc; fi
+echo -e "export NAME=${null_resource.k8scluster.triggers.k8sc_name}" >> ~/.bashrc
+echo -e "export KOPS_STATE_STORE=s3://${null_resource.k8scluster.triggers.k8sc_s3b_name}" >> ~/.bashrc
 CMD
         interpreter = [ "/bin/bash", "-c" ] 
     }
 
     # Unset the exported variables on destroy 
+    # NOTE: echo "-e" for redirecting to the .bashrc is not used because it processes the \ in the file. 
+    # Also putting dbl quotes around the file names won't work as bash can't expand the ~ charcter.    
     provisioner "local-exec" {
         when = "destroy"
         command = <<CMD
-unset NAME; unset KOPS_STATE_STORE
 mv ~/.bashrc ~/.bashrc-kubernetes.bak
 touch ~/.bashrc
 while IFS= read -r line; do
 	echo -e "$line" | grep "NAME" > /dev/null; if((!$?)); then continue; fi
 	echo -e "$line" | grep "KOPS_STATE_STORE" > /dev/null; if((!$?)); then continue; fi
 	echo "$line" >> ~/.bashrc
-done < "~/.bashrc-kubernetes.bak"
+done < ~/.bashrc-kubernetes.bak
 if (($?)); then 
     echo -e "Error updating ~/.bashrc. Restoring backup"; mv ~/.bashrc-kubernetes.bak ~/.bashrc
 else
 	echo -e "Removing ~/.bashrc-kubernetes.bak"; rm ~/.bashrc-kubernetes.bak
 fi
+unset NAME; unset KOPS_STATE_STORE
 CMD
         interpreter = [ "/bin/bash", "-c" ]
     }
