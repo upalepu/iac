5c9caf5cdc444bd40ff3ecf3e242cad661451715
Uma Palepu <umap@crosslaketech.com>
2018-06-22 18:00:24 -0700

    Refactored to remove references to windows. This tf module only creates linux EC2 machines. Cut variable name sizes to smaller, yet readable sizes.

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,5 +1,5 @@
 # This is a module file which describes an aws ec2 instance
-# It can create multiple Windows or Linux EC2 machines in AWS
+# It can create multiple Linux EC2 machines in AWS
 # It assumes that the provider is specified in the calling parent
 # Multiple volumes can be attached to the created EC2
 
@@ -23,14 +23,13 @@ variable "private_key_path" {
     description = <<DESCRIPTION
 Path to the private key for administrative SSH login from your remote machine.
 In Linux systems this typically is ~/.ssh/id_rsa.pem
-In Windows systems you could either use WSL (Ubuntu bash under windows) or git-bash or equivalent.
 DESCRIPTION
 }
 variable "public_key_path" {
 	description = <<DESCRIPTION
 Path to the SSH public key to be used for authentication.
 Ensure this keypair is added to your local SSH agent so provisioners can
-connect. Example: ~/.ssh/terraform.pub
+connect. Example: ~/.ssh/id_rsa.pub
 DESCRIPTION
 }
 variable "key_name" {
@@ -53,9 +52,6 @@ variable "sg_ids" {
 variable "subnet_id" {
     description = "Subnet id of the VPC into which the AMI is launched"
 }
-variable "platform" {
-    description = "This is the variable that determines whether Linux or Windows EC2 is created."
-}
 variable "ver" {
     description = <<DESCRIPTION
 Server OS version info. 
@@ -64,13 +60,8 @@ For Linux since we are only using ubuntu variants, the valid versions are
 The default setting is 16 (ubuntu 16.04)
 NOTE: That 12.04 is no longer supported by the ubuntu org, so it is recommended that
 ubuntu 16.04 is used. 
-For Windows, the version numbers are 2012R2 and 2016 corresponding to
-Windows 2012 R2 Server and Windows 2016 Server.  
 DESCRIPTION
-}
-variable "db" {
-    description = "Is MS SQL Server Standard or Enterprise installed. Valid values are [ssql|esql|none]."
-    default = "none"
+    default = "16"
 }
 variable "username" {
     description = <<DESCRIPTION
@@ -78,8 +69,6 @@ User name for administrative SSH login to the EC2 instance.
 This username is used by terraform when provisioning the EC2 instance using the provisioners.
 For Linux, this normally is whatever the default user account on the EC2 is
 for example: on ubuntu it is "ubuntu" on Amazon linux it is ec2-user etc. 
-For Windows, this normally is the "Administrator". NOTE: Currently this version of terraform config
-has no provisioners configured.  
 DESCRIPTION
 }
 variable "root_volume" {
@@ -88,11 +77,22 @@ variable "root_volume" {
 }
 variable "additional_volumes" {
     type = "list"
-    description = "Volumes (in addition to root volume) attached to the EC2 instance."
+    description = <<DESCRIPTION
+The following list of volume maps provide the data for the additional disks of the EC2 if needed.
+Valid device_name values are "/dev/xvd[e-z]"
+Valid type values are "gp2", "io1", "st1", "sc1" - but "gp2" is recommended in general as it has the best ROI.
+Valid size values are - 8GB - 16TB. Linux workloads need at least 8GB minimum for testing. 
+    { device_name = "/dev/xvde", type = "gp2", size = 8 },
+    { device_name = "/dev/xvdf", type = "gp2", size = 20 }
+DESCRIPTION
 }
 variable "files_to_copy" {
-    description = "Map of files to be copied to EC2 instance"
-    default = [{ source = "", destination = "" }]
+    type =  "list"
+  description = <<DESCRIPTION
+List of file copy Maps to be copied to EC2 instance
+  {source = "myfile.txt", destination = "~/myfile.txt" }, 
+  {source = "./myfolder", destination = "~/myfolder" } 
+DESCRIPTION
 }
 variable "remote_commands" {
     type = "list"
@@ -100,34 +100,17 @@ variable "remote_commands" {
 }
 variable "amis" {
     description = <<DESCRIPTION
-AMIs for both Linux and Windows based on region. NOTE That the keys in this map
-are coded differently between Windows and Linux.
-For Windows the coding of the key is <ver>-<db>-<region>. 
-The db portion is to accomodate SQL server - Enterprise or Standard.
-Its value will be esql for Enterprise SQL Server, ssql for Standard SQL Server and none for a
-machine which doesn't have SQL server. If you want to install your own version of SQL Server,
-you can use the none option of the database and provision the machine separately.     
-For Linux the coding of the key is <ver>-<region>. You can install any DB you want through the
-provisioners. 
+AMIs for Linuxbased on region. For Linux the coding of the key is <ver>-<region>. 
 NOTE: The AMIs in this list are all picked with the above attributes.
 DESCRIPTION
     default = { 
         "12-us-east-1" = "ami-a04529b6"
         "14-us-east-1" = "ami-c29e1cb8"
         "16-us-east-1" = "ami-aa2ea6d0"
-        "2012R2-sqle-us-east-1" = "ami-75493d0f"
-        "2012R2-ssql-us-east-1" = "ami-eeb5c194"
-        "2012R2-esql-us-east-1" = "ami-ab4733d1"
-        "2012-none-us-east-1" = "ami-455f2b3f"
-        "2012R2-none-us-east-1" = "ami-e443379e"
-        "2016-esql-us-east-1" = "ami-63acd819"
-        "2016-ssql-us-east-1" = "ami-b9a3d7c3"
-        "2016-none-us-east-1" = "ami-4096e23a"
     }
 }
 locals {
-    type = "${var.platform == "windows" ? "winrm":"ssh"}"
-    ami_key = "${var.platform == "windows" ? "${var.ver}-${var.db}-${var.region}" : "${var.ver}-${var.region}" }"
+    ami_key = "${var.ver}-${var.region}"
 }
 resource "aws_instance" "ec2" {
     count = "${var.count}"
@@ -144,46 +127,41 @@ resource "aws_instance" "ec2" {
     tags {
         Name = "${var.machine_name}-${count.index}"
         Project = "${var.project}"
-        Platform = "${title(var.platform)}"
+        Platform = "Linux"
     }
 }
 
-locals {
-    _provision_ec2 = "${var.platform == "windows" ? 0 : 1}"    
-    provision_ec2 = "${var.count > 0 ? (local._provision_ec2 * var.count) : 0 }"  # No provisioning if resource count is zero. 
-}
-# Used for provisioning of commands. Currently on windows systems we need password.  
-resource "null_resource" "provisioning" {
-    depends_on = [ "aws_instance.ec2", "null_resource.file_copy" ]
-    count = "${local.provision_ec2}"    # This will be > 0 if Linux & 0 if Windows
+# Used for provisioning of linux commands.  
+resource "null_resource" "lrcmd" {
+    depends_on = [ "aws_instance.ec2", "null_resource.lfcp" ]
+    count = "${var.count}"  
     triggers {
-        provisioning_ec2_id = "${element(aws_instance.ec2.*.id,count.index)}"
-        filecopy = "${join(",",null_resource.file_copy.*.id)}" 
+        ec2_id = "${element(aws_instance.ec2.*.id,count.index)}"
+        filecopy = "${join(",",null_resource.lfcp.*.id)}" 
     }
     connection {
         host = "${element(aws_instance.ec2.*.public_ip,count.index)}"
-        type = "${local.type}"
+        type = "ssh"
         user = "${var.username}" 
         private_key = "${file(var.private_key_path)}" 
     }
     provisioner "remote-exec" {
         inline = "${var.remote_commands}"
-        //on_failure = "continue" 
     }
 }
 
 locals {
-    _filecopy = "${lookup(var.files_to_copy[0],"source") == "" ? 0 : 1 }"
-    _filecopy_count = "${local._filecopy == 1 ? length(var.files_to_copy) : 0 }"
-    filecopy_count = "${var.count > 0 ? (local._filecopy_count * var.count) : 0 }"    # No file copy if resource count is zero.
-    filecopy_count_divisor = "${local._filecopy_count}"
+    _fcp = "${length(var.files_to_copy)}"
+    _fcp_count = "${local._fcp > 0 ? length(var.files_to_copy) : 0 }"
+    fcp = "${var.count > 0 ? (local._fcp_count * var.count) : 0 }"    # No file copy if resource count is zero.
+    fcp_divisor = "${local._fcp_count}"
 }
 # Used for copying files to the EC2. Currently we use this on Linux only.  
-resource "null_resource" "file_copy" {
+resource "null_resource" "lfcp" {
     depends_on = [ "aws_instance.ec2" ]
-    count = "${local.filecopy_count}"    # This will be > 0 if we need to copy files to the remote resource
+    count = "${local.fcp}"    # This will be > 0 if we need to copy files to the remote resource
     triggers {
-        provisioning_ec2_id = "${element(aws_instance.ec2.*.id,count.index)}"
+        ec2_id = "${element(aws_instance.ec2.*.id,count.index)}"
     }
     connection {
         host = "${element(aws_instance.ec2.*.public_ip,count.index)}"
@@ -192,28 +170,28 @@ resource "null_resource" "file_copy" {
         private_key = "${file(var.private_key_path)}" 
     }
     provisioner "file" {
-        source = "${lookup(var.files_to_copy[(count.index % local.filecopy_count_divisor)],"source")}"
-        destination = "${lookup(var.files_to_copy[(count.index % local.filecopy_count_divisor)],"destination")}"
+        source = "${lookup(var.files_to_copy[(count.index % local.fcp_divisor)],"source")}"
+        destination = "${lookup(var.files_to_copy[(count.index % local.fcp_divisor)],"destination")}"
     }
 }
 
 locals {
-    _additional_volume_count = "${length(var.additional_volumes)}" 
-    additional_volume_count = "${var.count > 0 ? (local._additional_volume_count * var.count) : 0 }" # No additional volumes if count is zero
-    additional_volume_count_divisor = "${local._additional_volume_count}"
+    _addlvol_count = "${length(var.additional_volumes)}" 
+    addlvol_count = "${var.count > 0 ? (local._addlvol_count * var.count) : 0 }" # No additional volumes if count is zero
+    addlvol_count_divisor = "${local._addlvol_count}"
 }
 # Used for creating additional volumes on the EC2. Data is supplied in the additional_volumes array
 resource "aws_ebs_volume" "vebs" {
-    count = "${local.additional_volume_count}"
+    count = "${local.addlvol_count}"
     availability_zone = "${element(aws_instance.ec2.*.availability_zone,count.index)}"
-    type = "${lookup(var.additional_volumes[(count.index % local.additional_volume_count_divisor)],"type")}"
-    size = "${lookup(var.additional_volumes[(count.index % local.additional_volume_count_divisor)],"size")}"
+    type = "${lookup(var.additional_volumes[(count.index % local.addlvol_count_divisor)],"type")}"
+    size = "${lookup(var.additional_volumes[(count.index % local.addlvol_count_divisor)],"size")}"
 }
 
 # Attaches the created additional volumes on the EC2. Same data supplied in the additional_volumes array is used
 resource "aws_volume_attachment" "vattach" {
-    count = "${local.additional_volume_count}"
-    device_name = "${lookup(var.additional_volumes[(count.index % local.additional_volume_count_divisor)],"device_name")}"
+    count = "${local.addlvol_count}"
+    device_name = "${lookup(var.additional_volumes[(count.index % local.addlvol_count_divisor)],"device_name")}"
     instance_id = "${element(aws_instance.ec2.*.id,count.index)}"
     volume_id = "${element(aws_ebs_volume.vebs.*.id,count.index)}"
 }
