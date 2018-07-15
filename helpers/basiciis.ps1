79d874e6328cd73ff768611dab755ff2ebf143c7
Uma Palepu <umap@crosslaketech.com>
2018-07-14 22:37:51 -0700

    Removed the command to start as admin. Not needed. Also commented out IIS-NetFxExtensibility45 and ASPNET45 features.

--- helpers/basiciis.ps1
+++ helpers/basiciis.ps1
@@ -1,7 +1,4 @@
 # This script installs IIS and the features required to run asp.net applications
-
-Start-Process powershell -Verb runAs
-
 # * Make sure you run this script from an Admin Prompt!
 # * Make sure Powershell Execution Policy is bypassed to run these scripts:
 # * YOU MAY HAVE TO RUN THIS COMMAND PRIOR TO RUNNING THIS SCRIPT!
@@ -13,7 +10,7 @@ Enable-WindowsOptionalFeature -Online -FeatureName IIS-CommonHttpFeatures
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpErrors
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpRedirect
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationDevelopment
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-NetFxExtensibility45
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-NetFxExtensibility45
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-HealthAndDiagnostics
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpLogging
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-LoggingLibraries
@@ -32,20 +29,8 @@ Enable-WindowsOptionalFeature -Online -FeatureName IIS-StaticContent
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-DefaultDocument
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebSockets
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationInit
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-NetFxExtensibility45
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET45
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-NetFxExtensibility45
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET45
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIExtensions
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIFilter
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpCompressionStatic
-
-Import-Module WebAdministration 
-New-WebAppPool -name "NewWebSiteAppPool"  -force
-
-$appPool = Get-Item -name "NewWebSiteAppPool" 
-$appPool.processModel.identityType = "NetworkService"
-$appPool.enable32BitAppOnWin64 = 1
-$appPool | Set-Item
-md "c:\Web Sites\NewWebSite"
-
-# All on one line
-$site = $site = new-WebSite -name "NewWebSite" -PhysicalPath "c:\Web Sites\NewWebSite" -HostHeader "home2.west-wind.com" -ApplicationPool "NewWebSiteAppPool" -force
